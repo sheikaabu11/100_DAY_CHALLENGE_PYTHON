ab51145569c94964427fc02b9cf3767280c1fee2
sheikaabu11 <sheikaabuthaircyber11@gmail.com>
2026-06-10 23:22:06 +0530

    Find if odd or even

--- Day2.py
+++ Day2.py
@@ -1,16 +1,23 @@
-print("Find largest of 3 number")
-a = int(input(" Enter a "))
-b = int(input(" Enter b "))
-c = int(input(" Enter c "))
+# print("Find largest of 3 number")
+# a = int(input(" Enter a "))
+# b = int(input(" Enter b "))
+# c = int(input(" Enter c "))
 
-if a > b:
-    if a > c:
-        print(f" {a} is largest of 3 number")
-    else:
-        print(f"{c} is largest of 3 number")
+# if a > b:
+#     if a > c:
+#         print(f" {a} is largest of 3 number")
+#     else:
+#         print(f"{c} is largest of 3 number")
+# else:
+#     if b > c: 
+#         print(f" {b} is largest of 3 number")
+#     else:
+#         print(f" {c} is largest of 3 number")
+
+
+a = int(input(" Enter a number to find if its odd or even  : "))
+
+if a % 2 != 0:
+    print(f"{a} is odd")
 else:
-    if b > c: 
-        print(f" {b} is largest of 3 number")
-    else:
-        print(f" {c} is largest of 3 number")
-
+    print(f"{a} is even")
