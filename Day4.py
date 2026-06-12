9ba23fca723990d619ae972b04fac319d5531520
sheikaabu11 <sheikaabuthaircyber11@gmail.com>
2026-06-12 22:12:44 +0530

    Prime number

--- Day4.py
+++ Day4.py
@@ -1,9 +1,21 @@
 # Count Digit
 
-n = int(input("Enter the Number : "))
-count = 0
-while n != 0:
-    n //= 10
-    count += 1
+# n = int(input("Enter the Number : "))
+# count = 0
+# while n != 0:
+#     n //= 10
+#     count += 1
 
-print("Digits: ",count)
+# print("Digits: ",count)
+
+#  Check prime 
+n = int(input("Enter a Number : "))
+count = 0
+for i in range(1,n+1):
+    if n % i == 0:
+        count += 1
+
+if count == 2:
+    print("Prime")
+else:
+    print("Not Prime")
