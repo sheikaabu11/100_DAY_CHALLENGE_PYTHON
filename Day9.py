690211bc6950a7a551a3c1c6499c45e466f9ecd7
sheikaabu11 <sheikaabuthaircyber11@gmail.com>
2026-06-19 23:27:18 +0530

    Day 9

---
+++ Day9.py
@@ -0,0 +1,29 @@
+
+# Count word Frequency 
+# text = input("Enter Sentence : ")
+# words= text.split()
+# frequency = {}
+# for word in words:
+#     frequency[word] = frequency.get(word,0)+1
+
+# print(frequency)
+
+#Find Duplicate Character
+
+# text = input("Enter a word")
+# duplicates = {}
+# for ch in text:
+#     duplicates[ch] = duplicates.get(ch,0)+1
+
+
+# for ch,count in duplicates.items():
+#     if count > 1:
+#         print(ch,"appears",count,"times")
+
+
+# Remove Space from String 
+
+text = input("Enter a Sentence  : ")
+
+result = text.replace(" ","")
+print(result)
